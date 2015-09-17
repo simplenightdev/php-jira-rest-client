359afcaf6a53721fe31bcfd919de7d7784ce5f52
andrei <andrei@simplenight.com>
2015-09-17 20:09:56 +0300

    Hooks api basic support

---
+++ src/Support/Laravel/JiraHook.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace JiraRestApi\Support\Laravel;
+
+use Illuminate\Support\Facades\Facade;
+
+class JiraHook extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected static function getFacadeAccessor()
+    {
+        return "jirahookservice";
+    }
+}
